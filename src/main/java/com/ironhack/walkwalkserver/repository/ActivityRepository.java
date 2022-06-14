5cf7072311d6e213cb916e87066976f5506a0746
Anita224455 <amoralespuga@gmail.com>
2022-06-14 14:06:43 +0200

    endpoint added to get the activities chosen by an user

--- src/main/java/com/ironhack/walkwalkserver/repository/ActivityRepository.java
+++ src/main/java/com/ironhack/walkwalkserver/repository/ActivityRepository.java
@@ -1,9 +1,14 @@
 package com.ironhack.walkwalkserver.repository;
 
 import com.ironhack.walkwalkserver.model.Activity;
+import com.ironhack.walkwalkserver.model.Dog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+    List<Activity> findByCreatorId(Long creatorId);
+    List<Activity> findByAssignedId(Long assignedId);
 }
