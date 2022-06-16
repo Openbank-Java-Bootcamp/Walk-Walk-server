d404f245a90ac3702ed689dc492d602be6bcf408
Anita224455 <amoralespuga@gmail.com>
2022-06-16 17:42:41 +0200

    add date calendar

--- src/main/java/com/ironhack/walkwalkserver/DTO/ActivityWithoutDogsDTO.java
+++ src/main/java/com/ironhack/walkwalkserver/DTO/ActivityWithoutDogsDTO.java
@@ -1,8 +1,11 @@
 package com.ironhack.walkwalkserver.DTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 
 @Data
@@ -14,6 +17,8 @@ public class ActivityWithoutDogsDTO {
     private String city;
     private Long creatorId;
     private Long assignedId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime activityDate;
     private boolean isChosen = false;
 
 }
